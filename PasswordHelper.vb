77b0fde31fff5a6f41ebea66eb22e75668470740
koter <42820377+vbaaccess@users.noreply.github.com>
2021-05-19 22:22:04 +0200

    Encryption with BlowFish.vb algorithm; algorithm + simple example
    Main files to be shredded in the application. 
    VB files should be placed in the VB language DLL project.
    The cs file contains an example of using the BlowFish.vb class

---
+++ PasswordHelper.vb
@@ -0,0 +1,46 @@
+﻿''' <summary>
+''' Klasa statyczna
+''' </summary>
+Public Module PasswordHelper
+
+    Public Function BlowFishEncryp(StringToEncrypt As String, YourKey As String) As String
+        Dim sRaw$, sRetStr$
+        Dim b As New BlowFish
+
+        Dim i As Integer
+
+        b.Password(YourKey)
+
+        sRaw = b.EncryptString(StringToEncrypt)
+
+        For i = 1 To Len(sRaw)
+            sRetStr = sRetStr & Right("0" & Hex(Asc(Mid(sRaw, i, 1))), 2)
+        Next i
+
+        BlowFishEncryp = sRetStr
+    End Function
+
+    Public Function BlowFishDecrypt(StringToDecrypt As String, YourKey As String) As String
+        Dim sR$, RetStr$
+        Dim b As New BlowFish
+
+        b.Password(YourKey)
+
+        Dim i As Integer
+
+        Dim sHex As String
+        Dim decHex As Integer
+        Dim sChr As String
+
+        For i = 1 To (Len(StringToDecrypt) / 2)
+            sHex = Mid$(StringToDecrypt, i * 2 - 1, 2)
+            decHex = Convert.ToUInt16(sHex, 16)
+            sChr = Chr(decHex)
+            RetStr += sChr
+        Next i
+
+        sR = b.DecryptString(RetStr)
+
+        BlowFishDecrypt = sR
+    End Function
+End Module
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+using VbLegacy;
+
+namespace EncryptionF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Encryption using algorytm BlowFish.vb ");
+
+            string Mykey = "DowolnyCiagZnakowKtoryJestKluczemPotzrebnymDoOdszyfrowaniaWiadomosciWczesniejZaszyfrowanejTymKluczem.ImCiagJestDluzszyTymChybaLepiej";
+            string MyMessage = "";
+            string DecryptMsg = "";
+            string EncryptMsg = "";
+           
+            // Test 1 - Szyfrowania
+            
+            MyMessage = "Siala baba mak, nie wiedziala jak";
+            EncryptMsg = "";
+            
+            EncryptMsg = PasswordHelper.BlowFishEncryp(MyMessage, Mykey);
+            Console.WriteLine("\nSTEP 1.1 - My Message: {0}", MyMessage);
+            Console.WriteLine("\nSTEP 1.2 - Encrypt Message: {0}", EncryptMsg);
+
+            // Test 2 - Deszyfrowania
+            EncryptMsg = "44FE287FD1DCC6F90B4CC7C6E94F0698F0ADF7EF99AE1C44489D99BC36C3C52F789687B86B5C69F6";
+            DecryptMsg = "";
+
+            DecryptMsg = PasswordHelper.BlowFishDecrypt(EncryptMsg, Mykey);
+            Console.WriteLine("\nSTEP 2.1 - Encrypt Message: {0} ...", EncryptMsg.Substring(1, 10));
+            Console.WriteLine("\nSTEP 2.2 - Decrypt Message: {0}", DecryptMsg);
+
+            // koniec - czekaj na ENTER
+            Console.WriteLine("\nPRESS ENTER to END");
+            Console.ReadLine();
+        }
+    }
+}
